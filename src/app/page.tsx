7b8f6a350a0b5826b2fd8b63e981f555436766f1
isehou <zaku47shift@gmail.com>
2024-10-20 01:38:37 +0600

    add: new components, fix: some bugs

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -13,6 +13,18 @@ export default function Home() {
         <Title text="Все пиццы" size="lg" className="font-extrabold" />
       </Container>
       <TopBar />
+
+      <Container className="pb-14">
+        <div className="flex gap-[60px]">
+          {/* Filtration */}
+          <div className="w-[250px]">{/* <Filters /> */}</div>
+
+          {/* Products list */}
+          <div className="flex-1">
+            <div className="flex flex-col gap-16">Список товаров</div>
+          </div>
+        </div>
+      </Container>
     </>
   );
 }
